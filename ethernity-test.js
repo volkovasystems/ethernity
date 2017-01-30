dc4bdfb5cd97cbf0a2c61abd5b5522531149d7c4
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-30 17:27:53 +0800

    Updated test.

--- ethernity-test.js
+++ ethernity-test.js
@@ -2,4 +2,32 @@
 
 const Ethernity = require( "./ethernity.js" );
 
-console.log( Ethernity( ) );
+let sample1 = new Date( "8/15/2016 12:47:45 PM" );
+
+console.log( Ethernity( sample1 ).printTime( ) );
+
+console.log( Ethernity( sample1 ).getTime( ) );
+
+console.log( Ethernity( sample1 ).getDate( ) );
+
+console.log( Ethernity( sample1 ).realTime( ) );
+
+console.log( Ethernity( sample1 ).relativeTime( ) );
+
+let ethernity_time = Ethernity( sample1 ).trueTime;
+
+console.log( "True time", ethernity_time );
+
+let compact1 = Ethernity( sample1 ).compact( );
+
+console.log( "Compact", compact1 );
+
+let comparison1 = Ethernity( ethernity_time ).parse( );
+
+console.log( "Comparison1", comparison1  );
+
+let comparison2 = Ethernity( compact1 ).parse( );
+
+console.log( "Comparison2", comparison2 );
+
+console.log( comparison1.trueTime === comparison2.trueTime );
